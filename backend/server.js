139c0eb2196183d8a3511191a75dc348db9237b1
Joyce Aguiar <joyceaguiar777@gmail.com>
2025-05-12 17:44:40 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,33 @@ app.use(express.json());
 app.use(cors());
 
 const fs = require('fs');
-const RANKINGS_FILE ='rankings.json';
+const RANKINGS_FILE = 'rankings.json';
 
 app.get('/rankings', (req, res) => {
     const data = fs.readFileSync(RANKINGS_FILE);
     const rankings = JSON.parse(data);
     res.json(rankings);
+});
+
+app.listen(3001, () => {
+    console.log("✨ Servidor rodando em http://localhost:3001")
+});
+
+app.post('/rankings', (req, res) => {
+    const {nome, nivel, tentativas} = req.body;
+
+    const novoRegistro = {
+        nome,
+        nivel,
+        tentativas,
+        data: new Date().toISOString()
+    };
+
+    const data = fs.readFileSync(RANKINGS_FILE);
+    const rankings = JSON.parse(data);
+
+    rankings.push(novoRegistro);
+
+    fs.writeFileSync(RANKINGS_FILE, JSON.stringify(rankings, null, 2));
+    res.status(201).json({status: 'ok'});
 });
