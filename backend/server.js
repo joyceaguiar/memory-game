b2513d00e145f4f37f922bdbe5198646731c9485
Joyce Aguiar <joyceaguiar777@gmail.com>
2025-05-12 17:32:29 -0300

    criando o backend

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const fs = require('fs');
+const RANKINGS_FILE ='rankings.json';
+
+app.get('/rankings', (req, res) => {
+    const data = fs.readFileSync(RANKINGS_FILE);
+    const rankings = JSON.parse(data);
+    res.json(rankings);
+});
